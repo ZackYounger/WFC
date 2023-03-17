bef021e009c505e87bcb787e97ab66648ffee6a5
zackyounger_mac <zack5@hotmail.com>
2023-03-17 15:47:40 +0000

    Clean up

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 with open('pipeline/adjacencyData.json', 'r') as f:
     adjacencyData = json.load(f)
 
-@app.route('/')
-def index():
-    return render_template("main-page.html")
-
 @app.route('/editor')
 def editor():
     return render_template("editor.html", adjPassData=adjacencyData)
@@ -16,14 +12,6 @@ def editor():
 @app.route('/testPage')
 def testPage():
     return render_template("testPage.html", adjPassData=adjacencyData)
-
-@app.route('/testPageTwo')
-def testPageTwo():
-    return render_template("testPageTwo.html", adjPassData=adjacencyData)
-
-@app.route('/fire')
-def fire():
-    return render_template("fire.html")
 
 if __name__ == "__main__":
     app.run(port=5004)
